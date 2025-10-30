be127d0fc916aef65eb2b31111325f6fa18d6822
seanjmcavoy <208255043+seanjmcavoy@users.noreply.github.com>
2025-10-30 21:43:08 +1300

    fix

--- Assets/Scripts/Tiles/GlassTile.cs
+++ Assets/Scripts/Tiles/GlassTile.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 [AddComponentMenu("PathQueue/Tiles/Glass Tile")]
 public class GlassTile : Tile
 {
-    private bool broken = false;
     public override IEnumerator OnEnter(PlayerController player, Direction fromDir)
     {
-        Debug.Log("Glasss");
         player.ResetToStart();
+        Debug.Log("Glasss");
         yield break;
     }
 }
